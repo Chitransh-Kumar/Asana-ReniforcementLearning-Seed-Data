82412dc35d036ed436a08ad9427e58c2fad41533
Chitransh-Kumar <kumar.chitransh1103@gmail.com>
2026-01-07 00:16:02 +0530

    Modified dates.py

--- src/utils/dates.py
+++ src/utils/dates.py
@@ -1,11 +1,24 @@
 import random
 from datetime import datetime, timedelta, timezone
 
-def random_past_datetime(min_days_ago: int, max_days_ago: int):
+def random_past_date(min_days_ago: int, max_days_ago: int):
     days_ago = random.randint(min_days_ago, max_days_ago)
     seconds_offset = random.randint(0, 86400)
 
-    return datetime.now(timezone.utc) - timedelta(
+    dt = datetime.now(timezone.utc) - timedelta(
         days=days_ago,
         seconds=seconds_offset
     )
+
+    return dt.date()
+
+def random_past_timestamp(min_days_ago: int, max_days_ago: int):
+    days_ago = random.randint(min_days_ago, max_days_ago)
+    seconds_offset = random.randint(0, 86400)
+
+    dt = datetime.now() - timedelta(
+        days=days_ago,
+        seconds=seconds_offset
+    )
+
+    return dt.replace(microsecond=0)
